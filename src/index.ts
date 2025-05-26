dec7e5b17e4f7d4047d61c29f8955ef0c989e135
keisokoo <keisokoo@gmail.com>
2025-05-26 15:49:34 +0900

    fix, play ground

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import cors from 'cors';
-import 'dotenv/config';
-import express from 'express';
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import playRouter from "./routes/play";
 
 const app = express();
 const port = 3080;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get('/', async (req, res) => {
-  res.send('Hello World!');
-})
+app.get("/", async (req, res) => {
+  res.send("Hello World!");
+});
+app.use("/play", playRouter);
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
